6483da9c97170f70d8331ce78656508f010f34e6
Niall McGuinness <niall.mcguinness@dkit.ie>
2023-10-09 12:29:58 +0100

    :sparkles: Changed SORotationBeh to use references

--- Assets/GD/Examples/1. Using Scriptable Objects - Shared Variables/Scripts/SORotationBehaviour.cs
+++ Assets/GD/Examples/1. Using Scriptable Objects - Shared Variables/Scripts/SORotationBehaviour.cs
@@ -1,9 +1,31 @@
+using GD;
 using UnityEngine;
 
 /// <summary>
 /// Performs axis, angle rotation of a game object via centrally controlled ScriptableObject (SO) variables
 /// </summary>
 public class SORotationBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private FloatReference rotationAngle;
+
+    [SerializeField]
+    private Vector3Reference rotationAxis;
+
+    [SerializeField]
+    private Space rotationSpace;
+
+    // Update is called every frame, if the MonoBehaviour is enabled
+    private void Update()
+    {
+        gameObject.transform.Rotate(rotationAxis.Value,
+            rotationAngle.Value,
+            rotationSpace);
+    }
+}
+
+/*
+ public class SORotationBehaviour : MonoBehaviour
 {
     [SerializeField]
     private RotationParameters rotationParameters;
@@ -19,3 +41,4 @@ public class SORotationBehaviour : MonoBehaviour
             rotationSpace);
     }
 }
+ */
