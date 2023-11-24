dcc39fce2541005680f4b5e8fee24627a1fd089b
Niall McGuinness <niall.mcguinness@dkit.ie>
2023-11-24 16:01:40 +0000

    :truck: Tidied flicker controller

--- Assets/TextFlickerController.cs
+++ Assets/TextFlickerController.cs
@@ -1,3 +1,4 @@
+using Sirenix.OdinInspector;
 using System.Collections;
 using TMPro;
 using UnityEngine;
@@ -6,7 +7,16 @@ public class TextFlickerController : MonoBehaviour
 {
     public TextMeshPro textMeshPro;
     public string text;
-    public float glowPower;
+
+    [Range(0, 1)]
+    public float minPower = 0;
+
+    [Range(0, 1)]
+    public float maxPower = 1;
+
+    [Range(0.1f, 5)]
+    [Unit(Units.Second)]
+    public float waitSeconds = 0.2f;
 
     private Material material;
 
@@ -15,16 +25,34 @@ public class TextFlickerController : MonoBehaviour
         material = textMeshPro.fontMaterial;
         textMeshPro.text = text;
 
-        StartCoroutine(DoFlicker());
+        var myRoutine = StartCoroutine(DoFlicker());
+
+        //    StopCoroutine(myRoutine);
+        //    StopAllCoroutines();
     }
 
     private IEnumerator DoFlicker()
     {
         while (true)
         {
-            var randPower = Random.Range(0.2f, 0.7f);
+            var randPower = Random.Range(minPower, maxPower);
             material.SetFloat("_GlowPower", randPower);
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(waitSeconds);
         }
+    }
+
+    private IEnumerator DoSomethingOverTime()
+    {
+        yield return new WaitForSeconds(2);
+
+        ///   System.Threading.Thread.Sleep(2);
+
+        Debug.Log("do something...");
+
+        yield return new WaitForSeconds(5);
+
+        Debug.Log("do something else...");
+
+        yield return null;
     }
 }
